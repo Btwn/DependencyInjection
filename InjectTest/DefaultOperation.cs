d575e15d1ec16f55c7cf1d351160f5f090429ae9
Carlos Alberto Diaz Jimenez <ca.diaz@mavi.mx>
2022-10-26 12:35:20 -0500

    init

---
+++ InjectTest/DefaultOperation.cs
@@ -0,0 +1,13 @@
+﻿using static System.Guid;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectTest
+{
+    public class DefaultOperation : ITransientOperation, IScopedOperation, ISingletonOperation
+    {
+        public string OperationId { get; } = NewGuid().ToString()[^4..];
+    }
+}
